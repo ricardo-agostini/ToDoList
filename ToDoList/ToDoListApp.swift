5a52f5868a594e5d60c7d4691f4ceb1956254e97
Ricardo de Aagostini <ricardoagneto@gmail.com>
2023-12-04 15:42:42 -0300

    first commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ricardo de Agostini Neto on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
